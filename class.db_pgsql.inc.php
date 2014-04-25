1594d6095c9390e2e76f3514290042d98279b562
Joe Huss <detain@interserver.net>
2014-04-25 08:39:50 +0000

    updates

--- class.db_pgsql.inc.php
+++ class.db_pgsql.inc.php
@@ -4,8 +4,8 @@
  * PostgreSQL Related Functionality
  * @author Joe Huss <detain@corpmail.interserver.net>
  * @package MyAdmin
- * @subpackage MyAdmin
- * @subpackage SQL
+
+SQL
  * @copyright 2010
  */
 
@@ -27,8 +27,8 @@
  * db
  * 
  * @package MyAdmin
- * @subpackage MyAdmin
- * @subpackage   
+
+
  * @author cpaneldirect
  * @copyright Owner
  * @version 2011
