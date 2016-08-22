1f975124486234a7320a0711852428f399af2b82
Joe Huss <detain@interserver.net>
2016-08-22 12:40:24 +0000

    Formatting Updates for db/class.db_mysqli.inc.php

--- class.db_mysqli.inc.php
+++ class.db_mysqli.inc.php
@@ -88,8 +88,7 @@
 		 * @param string $Host Optional The hostname where the server is, or default to localhost
 		 * @param string $query Optional query to perform immediately
 		 */
-		public function __construct($Database = '', $User = '', $Password = '', $Host = 'localhost', $query = '')
-		{
+		public function __construct($Database = '', $User = '', $Password = '', $Host = 'localhost', $query = '') {
 			$this->Database = $Database;
 			$this->User = $User;
 			$this->Password = $Password;
@@ -104,8 +103,7 @@
 		 * @param string $line
 		 * @param string $file
 		 */
-		public function log($message, $line = '', $file = '')
-		{
+		public function log($message, $line = '', $file = '') {
 			if (function_exists('billingd_log'))
 				billingd_log($message, $line, $file, false);
 			else
@@ -117,8 +115,7 @@
 		 * db::link_id()
 		 * @return int
 		 */
-		public function link_id()
-		{
+		public function link_id() {
 			return $this->Link_ID;
 		}
 
@@ -126,8 +123,7 @@
 		 * db::query_id()
 		 * @return int
 		 */
-		public function query_id()
-		{
+		public function query_id() {
 			return $this->Query_ID;
 		}
 
@@ -140,28 +136,22 @@
 		 * @param string $Password
 		 * @return int|\mysqli
 		 */
-		public function connect($Database = '', $Host = '', $User = '', $Password = '')
-		{
+		public function connect($Database = '', $Host = '', $User = '', $Password = '') {
 			/* Handle defaults */
-			if ('' == $Database)
-			{
+			if ('' == $Database) {
 				$Database = $this->Database;
 			}
-			if ('' == $Host)
-			{
+			if ('' == $Host) {
 				$Host = $this->Host;
 			}
-			if ('' == $User)
-			{
+			if ('' == $User) {
 				$User = $this->User;
 			}
-			if ('' == $Password)
-			{
+			if ('' == $Password) {
 				$Password = $this->Password;
 			}
 			/* establish connection, select database */
-			if (!is_object($this->Link_ID))
-			{
+			if (!is_object($this->Link_ID)) {
 				$this->connection_atttempt++;
 				if ($this->connection_atttempt > 1)
 					billingd_log("MySQLi Connection Attempt #{$this->connection_atttempt}/{$this->max_connect_errors}", __LINE__, __FILE__);
@@ -191,8 +181,7 @@
 		 * db::disconnect()
 		 * @return int
 		 */
-		public function disconnect()
-		{
+		public function disconnect() {
 			if (is_object($this->Link_ID))
 				return $this->Link_ID->close();
 			else
@@ -203,10 +192,8 @@
 		 * @param $string
 		 * @return string
 		 */
-		public function real_escape($string)
-		{
-			if ((!is_resource($this->Link_ID) || $this->Link_ID == 0) && !$this->connect())
-			{
+		public function real_escape($string) {
+			if ((!is_resource($this->Link_ID) || $this->Link_ID == 0) && !$this->connect()) {
 				return mysqli_escape_string($string);
 			}
 			return mysqli_real_escape_string($this->Link_ID, $string);
@@ -216,8 +203,7 @@
 		 * @param $string
 		 * @return string
 		 */
-		public function escape($string)
-		{
+		public function escape($string) {
 			return mysql_escape_string($string);
 		}
 
@@ -226,10 +212,8 @@
 		 * @param mixed $str
 		 * @return string
 		 */
-		public function db_addslashes($str)
-		{
-			if (!isset($str) || $str == '')
-			{
+		public function db_addslashes($str) {
+			if (!isset($str) || $str == '') {
 				return '';
 			}
 
@@ -241,8 +225,7 @@
 		 * @param mixed $epoch
 		 * @return bool|string
 		 */
-		public function to_timestamp($epoch)
-		{
+		public function to_timestamp($epoch) {
 			return date('YmdHis', $epoch);
 		}
 
@@ -251,8 +234,7 @@
 		 * @param mixed $timestamp
 		 * @return bool|int|mixed
 		 */
-		public function from_timestamp($timestamp)
-		{
+		public function from_timestamp($timestamp) {
 			if (preg_match('/([0-9]{4})-([0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2})/', $timestamp, $parts))
 				return mktime($parts[4], $parts[5], $parts[6], $parts[2], $parts[3], $parts[1]);
 			elseif (preg_match('/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/', $timestamp, $parts))
@@ -261,8 +243,7 @@
 				return mktime(1, 1, 1, $parts[2], $parts[3], $parts[1]);
 			elseif (is_numeric($timestamp) && $timestamp >= 943938000)
 				return $timestamp;
-			else
-			{
+			else {
 				$this->log('Cannot Match Timestamp from ' . $timestamp, __LINE__, __FILE__);
 				return false;
 			}
@@ -273,16 +254,12 @@
 		 * @param mixed $start
 		 * @return string
 		 */
-		public function limit($start)
-		{
+		public function limit($start) {
 			echo '<b>Warning: limit() is no longer used, use limit_query()</b>';
 
-			if ($start == 0)
-			{
+			if ($start == 0) {
 				$s = 'limit ' . $GLOBALS['phpgw_info']['user']['preferences']['common']['maxmatchs'];
-			}
-			else
-			{
+			} else {
 				$s = "limit $start," . $GLOBALS['phpgw_info']['user']['preferences']['common']['maxmatchs'];
 			}
 			return $s;
@@ -293,8 +270,7 @@
 		 * db::free()
 		 * @return void
 		 */
-		public function free()
-		{
+		public function free() {
 			if (is_resource($this->Query_ID))
 				@mysqli_free_result($this->Query_ID);
 			$this->Query_ID = 0;
@@ -311,23 +287,16 @@
 		 * @param string $file optionally pass __FILE__ calling the query for logging
 		 * @return mixed false if no rows, if a single row it returns that, if multiple it returns an array of rows, associative responses only
 		 */
-		public function query_return($query, $line = '', $file = '')
-		{
+		public function query_return($query, $line = '', $file = '') {
 			$this->query($query, $line, $file);
-			if ($this->num_rows() == 0)
-			{
+			if ($this->num_rows() == 0) {
 				return false;
-			}
-			elseif ($this->num_rows() == 1)
-			{
+			} elseif ($this->num_rows() == 1) {
 				$this->next_record(MYSQL_ASSOC);
 				return $this->Record;
-			}
-			else
-			{
+			} else {
 				$out = array();
-				while ($this->next_record(MYSQL_ASSOC))
-				{
+				while ($this->next_record(MYSQL_ASSOC)) {
 					$out[] = $this->Record;
 				}
 				return $out;
@@ -344,8 +313,7 @@
 		 * @param string $file optionally pass __FILE__ calling the query for logging
 		 * @return mixed false if no rows, if a single row it returns that, if multiple it returns an array of rows, associative responses only
 		 */
-		public function qr($query, $line = '', $file = '')
-		{
+		public function qr($query, $line = '', $file = '') {
 			return $this->query_return($query, $line, $file);
 		}
 
@@ -359,35 +327,29 @@
 		 * @param string $file
 		 * @return mixed 0 if no query or query id handler, safe to ignore this return
 		 */
-		public function query($Query_String, $line = '', $file = '')
-		{
+		public function query($Query_String, $line = '', $file = '') {
 			/* No empty queries, please, since PHP4 chokes on them. */
 			/* The empty query string is passed on from the constructor,
 			* when calling the class without a query, e.g. in situations
 			* like these: '$db = new db_Subclass;'
 			*/
-			if ($Query_String == '')
-			{
+			if ($Query_String == '') {
 				return 0;
 			}
-			if (!$this->connect())
-			{
+			if (!$this->connect()) {
 				return 0;
 				/* we already complained in connect() about that. */
 			}
 			$halt_prev = $this->Halt_On_Error;
 			$this->Halt_On_Error = 'no';
 			// New query, discard previous result.
-			if (is_resource($this->Query_ID))
-			{
+			if (is_resource($this->Query_ID)) {
 				$this->free();
 			}
-			if ($this->Debug)
-			{
+			if ($this->Debug) {
 				printf("Debug: query = %s<br>\n", $Query_String);
 			}
-			if (isset($GLOBALS['log_queries']) && $GLOBALS['log_queries'] !== false)
-			{
+			if (isset($GLOBALS['log_queries']) && $GLOBALS['log_queries'] !== false) {
 				$this->log($Query_String, $line, $file);
 			}
 			$tries = 3;
@@ -422,8 +384,7 @@
 				}
 			}
 			$this->Halt_On_Error = $halt_prev;
-			if (is_null($this->Query_ID) || $this->Query_ID === false)
-			{
+			if (is_null($this->Query_ID) || $this->Query_ID === false) {
 				$this->halt('', $line, $file);
 			}
 
@@ -441,23 +402,17 @@
 		 * @param string $num_rows
 		 * @return mixed
 		 */
-		public function limit_query($Query_String, $offset, $line = '', $file = '', $num_rows = '')
-		{
-			if (!$num_rows)
-			{
+		public function limit_query($Query_String, $offset, $line = '', $file = '', $num_rows = '') {
+			if (!$num_rows) {
 				$num_rows = $GLOBALS['phpgw_info']['user']['preferences']['common']['maxmatchs'];
 			}
-			if ($offset == 0)
-			{
+			if ($offset == 0) {
 				$Query_String .= ' LIMIT ' . $num_rows;
-			}
-			else
-			{
+			} else {
 				$Query_String .= ' LIMIT ' . $offset . ',' . $num_rows;
 			}
 
-			if ($this->Debug)
-			{
+			if ($this->Debug) {
 				printf("Debug: limit_query = %s<br>offset=%d, num_rows=%d<br>\n", $Query_String, $offset, $num_rows);
 			}
 
@@ -471,10 +426,8 @@
 		 * @param mixed $result_type
 		 * @return bool
 		 */
-		public function next_record($result_type = MYSQLI_BOTH)
-		{
-			if ($this->Query_ID === false)
-			{
+		public function next_record($result_type = MYSQLI_BOTH) {
+			if ($this->Query_ID === false) {
 				$this->halt('next_record called with no query pending.');
 				return 0;
 			}
@@ -485,8 +438,7 @@
 			$this->Error = mysqli_error($this->Link_ID);
 
 			$stat = is_array($this->Record);
-			if (!$stat && $this->Auto_Free && is_resource($this->Query_ID))
-			{
+			if (!$stat && $this->Auto_Free && is_resource($this->Query_ID)) {
 				$this->free();
 			}
 			return $stat;
@@ -498,15 +450,11 @@
 		 * @param integer $pos
 		 * @return int
 		 */
-		public function seek($pos = 0)
-		{
+		public function seek($pos = 0) {
 			$status = @mysqli_data_seek($this->Query_ID, $pos);
-			if ($status)
-			{
+			if ($status) {
 				$this->Row = $pos;
-			}
-			else
-			{
+			} else {
 				$this->halt("seek($pos) failed: result has " . $this->num_rows() . " rows");
 				/* half assed attempt to save the day,
 				* but do not consider this documented or even
@@ -523,8 +471,7 @@
 		 * db::transaction_begin()
 		 * @return bool
 		 */
-		public function transaction_begin()
-		{
+		public function transaction_begin() {
 			return true;
 		}
 
@@ -532,8 +479,7 @@
 		 * db::transaction_commit()
 		 * @return bool
 		 */
-		public function transaction_commit()
-		{
+		public function transaction_commit() {
 			return true;
 		}
 
@@ -541,8 +487,7 @@
 		 * db::transaction_abort()
 		 * @return bool
 		 */
-		public function transaction_abort()
-		{
+		public function transaction_abort() {
 			return true;
 		}
 
@@ -552,16 +497,14 @@
 		 * @param mixed $field
 		 * @return int|string
 		 */
-		public function get_last_insert_id($table, $field)
-		{
+		public function get_last_insert_id($table, $field) {
 			/* This will get the last insert ID created on the current connection.  Should only be called
 			* after an insert query is run on a table that has an auto incrementing field.  $table and
 			* $field are required, but unused here since it's unnecessary for mysql.  For compatibility
 			* with pgsql, the params must be supplied.
 			*/
 
-			if (!isset($table) || $table == '' || !isset($field) || $field == '')
-			{
+			if (!isset($table) || $table == '' || !isset($field) || $field == '') {
 				return - 1;
 			}
 
@@ -575,33 +518,24 @@
 		 * @param string $mode
 		 * @return bool|int|\mysqli_result
 		 */
-		public function lock($table, $mode = 'write')
-		{
+		public function lock($table, $mode = 'write') {
 			$this->connect();
 
 			$query = "lock tables ";
-			if (is_array($table))
-			{
-				while (list($key, $value) = each($table))
-				{
-					if ($key == "read" && $key != 0)
-					{
+			if (is_array($table)) {
+				while (list($key, $value) = each($table)) {
+					if ($key == "read" && $key != 0) {
 						$query .= "$value read, ";
-					}
-					else
-					{
+					} else {
 						$query .= "$value $mode, ";
 					}
 				}
 				$query = substr($query, 0, -2);
-			}
-			else
-			{
+			} else {
 				$query .= "$table $mode";
 			}
 			$res = @mysqli_query($this->Link_ID, $query);
-			if (!$res)
-			{
+			if (!$res) {
 				$this->halt("lock($table, $mode) failed.");
 				return 0;
 			}
@@ -613,13 +547,11 @@
 		 * @param bool $halt_on_error optional, deffaults to true, wether or not to halt on error
 		 * @return bool|int|\mysqli_result
 		 */
-		public function unlock($halt_on_error = true)
-		{
+		public function unlock($halt_on_error = true) {
 			$this->connect();
 
 			$res = @mysqli_query($this->Link_ID, "unlock tables");
-			if ($halt_on_error === true && !$res)
-			{
+			if ($halt_on_error === true && !$res) {
 				$this->halt("unlock() failed.");
 				return 0;
 			}
@@ -631,8 +563,7 @@
 		 * db::affected_rows()
 		 * @return int
 		 */
-		public function affected_rows()
-		{
+		public function affected_rows() {
 			return @mysqli_affected_rows($this->Link_ID);
 		}
 
@@ -640,8 +571,7 @@
 		 * db::num_rows()
 		 * @return int
 		 */
-		public function num_rows()
-		{
+		public function num_rows() {
 			return @mysqli_num_rows($this->Query_ID);
 		}
 
@@ -649,8 +579,7 @@
 		 * db::num_fields()
 		 * @return int
 		 */
-		public function num_fields()
-		{
+		public function num_fields() {
 			return @mysqli_num_fields($this->Query_ID);
 		}
 
@@ -659,8 +588,7 @@
 		 * db::nf()
 		 * @return int
 		 */
-		public function nf()
-		{
+		public function nf() {
 			return $this->num_rows();
 		}
 
@@ -668,8 +596,7 @@
 		 * db::np()
 		 * @return void
 		 */
-		public function np()
-		{
+		public function np() {
 			print $this->num_rows();
 		}
 
@@ -680,14 +607,10 @@
 		 * @param string $strip_slashes
 		 * @return string
 		 */
-		public function f($Name, $strip_slashes = "")
-		{
-			if ($strip_slashes || ($this->auto_stripslashes && !$strip_slashes))
-			{
+		public function f($Name, $strip_slashes = "") {
+			if ($strip_slashes || ($this->auto_stripslashes && !$strip_slashes)) {
 				return stripslashes($this->Record[$Name]);
-			}
-			else
-			{
+			} else {
 				return $this->Record[$Name];
 			}
 		}
@@ -698,8 +621,7 @@
 		 * @param mixed $Name
 		 * @return void
 		 */
-		public function p($Name)
-		{
+		public function p($Name) {
 			print $this->Record[$Name];
 		}
 
@@ -710,35 +632,28 @@
 		 * @param mixed $seq_name
 		 * @return int
 		 */
-		public function nextid($seq_name)
-		{
+		public function nextid($seq_name) {
 			$this->connect();
 
-			if ($this->lock($this->Seq_Table))
-			{
+			if ($this->lock($this->Seq_Table)) {
 				/* get sequence number (locked) and increment */
 				$q = sprintf("select nextid from %s where seq_name = '%s'", $this->Seq_Table, $seq_name);
 				$id = @$this->Link_ID->query($q);
 				$res = @$id->fetch_array();
 
 				/* No current value, make one */
-				if (!is_array($res))
-				{
+				if (!is_array($res)) {
 					$currentid = 0;
 					$q = sprintf("insert into %s values('%s', %s)", $this->Seq_Table, $seq_name, $currentid);
 					$id = @$this->Link_ID->query($q);
-				}
-				else
-				{
+				} else {
 					$currentid = $res["nextid"];
 				}
 				$nextid = $currentid + 1;
 				$q = sprintf("update %s set nextid = '%s' where seq_name = '%s'", $this->Seq_Table, $nextid, $seq_name);
 				$id = @$this->Link_ID->query($q);
 				$this->unlock();
-			}
-			else
-			{
+			} else {
 				$this->halt("cannot lock " . $this->Seq_Table . " - has it been created?");
 				return 0;
 			}
@@ -754,31 +669,26 @@
 		 * @param string $file
 		 * @return void
 		 */
-		public function halt($msg, $line = '', $file = '')
-		{
+		public function halt($msg, $line = '', $file = '') {
 			$this->unlock(false);
 			/* Just in case there is a table currently locked */
 
 			//$this->Error = @$this->Link_ID->error;
 			//$this->Errno = @$this->Link_ID->errno;
-			if ($this->Halt_On_Error == "no")
-			{
+			if ($this->Halt_On_Error == "no") {
 				return;
 			}
 			if ($msg != '')
 				$this->haltmsg($msg);
 
-			if ($file)
-			{
+			if ($file) {
 				error_log("File: $file");
 			}
-			if ($line)
-			{
+			if ($line) {
 				error_log("Line: $line");
 			}
 
-			if ($this->Halt_On_Error != "report")
-			{
+			if ($this->Halt_On_Error != "report") {
 				echo "<p><b>Session halted.</b>";
 				// FIXME! Add check for error levels
 				if (isset($GLOBALS['tf']))
@@ -792,11 +702,9 @@
 		 * @param mixed $msg
 		 * @return void
 		 */
-		public function haltmsg($msg, $line = '', $file = '')
-		{
+		public function haltmsg($msg, $line = '', $file = '') {
 			$this->log("Database error: $msg", $line, $file);
-			if ($this->Errno != "0" || !in_array($this->Error, '', "()"))
-			{
+			if ($this->Errno != "0" || !in_array($this->Error, '', "()")) {
 				$sqlstate = mysqli_sqlstate($this->Link_ID);
 				$this->log("MySQLi SQLState: {$sqlstate}. Error: " . $this->Errno . " (" . $this->Error . ")", $line, $file);
 			}
@@ -830,13 +738,11 @@
 		 *
 		 * @return array
 		 */
-		public function table_names()
-		{
+		public function table_names() {
 			$return = array();
 			$this->query("SHOW TABLES");
 			$i = 0;
-			while ($info = $this->Query_ID->fetch_row())
-			{
+			while ($info = $this->Query_ID->fetch_row()) {
 				$return[$i]['table_name'] = $info[0];
 				$return[$i]['tablespace_name'] = $this->Database;
 				$return[$i]['database'] = $this->Database;
@@ -850,8 +756,7 @@
 		 *
 		 * @return array
 		 */
-		public function index_names()
-		{
+		public function index_names() {
 			$return = array();
 			return $return;
 		}
@@ -863,14 +768,12 @@
 		 * @param string $adminpasswd
 		 * @return void
 		 */
-		public function create_database($adminname = '', $adminpasswd = '')
-		{
+		public function create_database($adminname = '', $adminpasswd = '') {
 			$currentUser = $this->User;
 			$currentPassword = $this->Password;
 			$currentDatabase = $this->Database;
 
-			if ($adminname != '')
-			{
+			if ($adminname != '') {
 				$this->User = $adminname;
 				$this->Password = $adminpasswd;
 				$this->Database = "mysql";
