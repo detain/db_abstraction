84688bb0e203807111d43e4629eca4ea38b282f2
Joe Huss <detain@interserver.net>
2016-08-23 05:04:16 +0000

    fixing somepaths

--- class.db_adodb.inc.php
+++ class.db_adodb.inc.php
@@ -59,8 +59,8 @@
 			if (!defined('_ADODB_LAYER')) {
 				if (file_exists('adodb5/adodb.inc.php'))
 					require_once ('adodb5/adodb.inc.php');
-				elseif (defined('INSTALL_ROOT') && file_exists(INSTALL_ROOT . '/include/database/adodb5/adodb.inc.php'))
-					require_once ('include/database/adodb5/adodb.inc.php');
+				elseif (defined('INSTALL_ROOT') && file_exists(INSTALL_ROOT . '/include/db/adodb5/adodb.inc.php'))
+					require_once ('include/db/adodb5/adodb.inc.php');
 				elseif (file_exists(realpath(dirname(__FILE__)) . '/../vendor/adodb/adodb-php/adodb.inc.php'))
 					require_once (realpath(dirname(__FILE__)) . '/../vendor/adodb/adodb-php/adodb.inc.php');
 			}
