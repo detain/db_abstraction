f6731c7c918da797cbd9cf455b522124955a7f9b
Joe Huss <detain@interserver.net>
2025-01-01 06:32:01 -0500

    updated copyright

--- src/Db_Interface.php
+++ src/Db_Interface.php
@@ -2,7 +2,7 @@
 /**
  * Generic SQL Driver Related Functionality
  * by detani@interserver.net
- * @copyright 2019
+ * @copyright 2025
  * @package MyAdmin
  * @category SQL
  */
--- src/Mdb2/Db.php
+++ src/Mdb2/Db.php
@@ -2,7 +2,7 @@
 /**
 * MDB2 Wrapper Made To Handle Like Our Other Classes Related Functionality
 * @author Joe Huss <detain@interserver.net>
-* @copyright 2019
+* @copyright 2025
 * @package MyAdmin
 * @category SQL
 */
